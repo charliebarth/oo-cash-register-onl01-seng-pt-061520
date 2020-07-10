82551c21b5ca134578b1badbf4388be926cbd6f8
Charlie Barth <charliepbarth@gmail.com>
2020-07-10 20:35:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,47 @@
-
+class CashRegister
+  attr_accessor :total, :title, :price, :quantity
+  @total = 0
+  @@items = []
+  @@item_prices = []
+  
+  def initialize(employee_discount = nil)
+    @employee_discount = employee_discount
+    @total = 0
+  end
+  
+  def discount
+    @employee_discount
+  end
+  
+  def add_item(title, price, quantity = 1)
+    @title = title
+    @price = price
+    @quantity = quantity
+    @total += price*quantity
+  end
+  
+  def apply_discount
+    if @employee_discount != 20
+      "There is no discount to apply."
+    else
+      first = @total * discount/100
+    @total = @total - first
+    "After the discount, the total comes to $#{@total}."
+    end
+  end
+  
+  def items
+    @@items.clear
+    add_item(title, price, quantity = 1)
+    @quantity.times do 
+      @@items << @title
+      @@item_prices << @price
+    end
+    @@items
+  end
+  
+  def void_last_transaction
+    count = @@item_prices.size - 1
+    @total - @@item_prices[count]
+  end
+end
---
+++ test.rb
@@ -0,0 +1,6 @@
+  def apply_discount
+    total = 1000
+    discount = 20
+  puts total * discount/100
+  end
+  apply_discount
